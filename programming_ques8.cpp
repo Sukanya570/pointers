439ae6c9ddcc473532f0aadb86991b7d108f05a6
Sukanya570 <Sukanyarpatil25@gmail.com>
2026-01-01 19:18:42 -0800

    Create programming_ques8.cpp

---
+++ programming_ques8.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<arr[n-1-j][i] <<" ";
+        }
+        cout<<endl;
+    }
+  
+        
+    
+    
+
+    return 0;
+}
